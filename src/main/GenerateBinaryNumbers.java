78c93eac5db50ad5790ad202dd2c8ec17b2db66c
Luís Carta <luis.carta@gmail.com>
2021-04-21 20:58:16 +0000

    Add generate binary numbers

---
+++ src/main/GenerateBinaryNumbers.java
@@ -0,0 +1,17 @@
+public class GenerateBinaryNumbers {
+    public static void main(String[] args) {
+        generateBinaryNumber(1);
+    }
+
+    private static void generateBinaryNumber(int i) {
+
+    }
+}
+
+// 1 -> 1
+// 2 -> 10
+// 3 -> 11
+// 4 -> 100
+// 5 -> 101
+// 6 -> 110
+// 7 -> 111
